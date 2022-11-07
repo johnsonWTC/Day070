262113b30f2801daa8685a6d24efbe27006f7e1a
/ohnsonWTC <johnsondubula@gmail.com>
2022-11-07 23:06:34 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Day070';
+  title = 'Day070 App';
+  innerText = "";
+
+getValue(){
+ let value  = confirm("do you want to show information?");
+ if(value){
+  this.innerText = "you choose yes"
+ }else{
+  this.innerText = "you choose no"
+ }
+}
+
 }
